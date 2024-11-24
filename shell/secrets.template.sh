d4e350889ffcda1e835f17c702fc03f98bb3be30
Jack Zampolin <jack.zampolin@gmail.com>
2024-11-24 11:12:02 -0800

    push

---
+++ shell/secrets.template.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+# Template for secrets - copy to secrets.sh and fill in values
+
+# Cloud Providers
+export AWS_ACCESS_KEY_ID=""
+export AWS_SECRET_ACCESS_KEY=""
+export GOOGLE_CLOUD_KEY=""
+export AZURE_KEY=""
+
+# Version Control
+export GITHUB_TOKEN=""
+export GITLAB_TOKEN=""
+
+# CI/CD
+export DOCKER_USERNAME=""
+export DOCKER_PASSWORD=""
+export NPM_TOKEN=""
+
+# APIs and Services
+export SLACK_API_TOKEN=""
+export STRIPE_API_KEY=""
+export SENDGRID_API_KEY=""
+export CLOUDFLARE_API_KEY=""
+
+# Development
+export OPENAI_API_KEY=""
+export HUGGINGFACE_TOKEN=""
+
+# Add your custom secrets below
